f4c5de714512609c9391c87033ff9e34bd60757a
Maxime Meurisse <max.meurisse@outlook.com>
2020-05-16 10:17:22 +0200

    File organisation

---
+++ 3/report/resources/c/pseudo-code-dp.c
@@ -0,0 +1,26 @@
+errMin(n, k, error, intervals, histogram, nLevels) {
+	err = 0
+	minErr = +INFINITY
+	index = getIndex(n, k, nLevels)
+
+	if(k == n)
+		error[index] = 0;
+		return 0;
+
+	if(k == 0)
+		error[index] = computeError(histogram, 0, n);
+		return error[index];
+
+	if(error[index] != +INFINITY)
+		return error[index];
+
+	for(i = n - 1; i > k - 1; i--)
+		err = computeError(histogram, i, n) + errMin(i, k - 1, error, intervals, histogram, nLevels);
+
+		if(err < minErr)
+			minErr = err;
+
+	error[index] = minErr;
+
+	return minErr;
+}
